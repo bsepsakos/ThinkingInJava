6561f5aca55912f145efa942a34ebbb349075651
bsepsakos <bsepsakos@gmail.com>
2016-02-03 21:47:00 +0200

    First Commit - Chapter 1

---
+++ ThinkingInJava/src/thinking/in/java/HelloDate.java
@@ -0,0 +1,9 @@
+package thinking.in.java;
+import java.util.*;
+
+public class HelloDate {
+	public static void printHelloDate() {
+		System.out.print("Hello, it's: ");
+		System.out.print(new Date());
+	}
+}
---
+++ ThinkingInJava/src/thinking/in/java/ThinkingInJavaMain.java
@@ -0,0 +1,17 @@
+/** The first Thinking in Java example program.
+* Displays a string and today’s date.
+* @author Bill Sepsakos
+* @version 1.0
+*/
+
+package thinking.in.java;
+//import thinking.in.java.*;
+
+public class ThinkingInJavaMain {
+/** test javadoc.
+* @param args array of string arguments
+*/
+public static void main(String args[]) {
+HelloDate.printHelloDate();
+	}
+}
